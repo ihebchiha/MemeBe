3161d00ed6e141810dac860cc982bb45371f9816
IhebChiha <ihebfcb1899@gmail.com>
2021-10-23 16:20:53 +0000

    adding new entities

--- src/main/java/com/gobeyond/memebe/domain/Meme.java
+++ src/main/java/com/gobeyond/memebe/domain/Meme.java
@@ -1,10 +1,43 @@
 package com.gobeyond.memebe.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Table(name = "t_meme")
 public class Meme {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GenericGenerator(name = "", strategy = "")
+    private String id;
+
+    @URL
+    private String memeUrl;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private Long rating;
+
+    @NotNull
+    private Long likes;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- src/main/java/com/gobeyond/memebe/domain/User.java
+++ src/main/java/com/gobeyond/memebe/domain/User.java
@@ -1,14 +1,18 @@
 package com.gobeyond.memebe.domain;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -24,6 +28,17 @@ public class User {
     @Email
     @NotBlank
     private String email;
+
+    @NotBlank
+    private String phoneNumber;
+
+    @NotBlank
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "")
+    private List<Meme> memes;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
