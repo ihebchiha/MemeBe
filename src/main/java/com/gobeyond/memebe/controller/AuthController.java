0909998c13012741d87d0b670ee7dac7b2e6dcfa
IhebChiha <ihebfcb1899@gmail.com>
2021-10-31 20:37:42 +0000

    updating controllers

--- src/main/java/com/gobeyond/memebe/controller/AuthController.java
+++ src/main/java/com/gobeyond/memebe/controller/AuthController.java
@@ -1,18 +1,16 @@
 package com.gobeyond.memebe.controller;
 
-import javax.validation.Valid;
-
 import com.gobeyond.memebe.domain.dto.TokenDto;
 import com.gobeyond.memebe.domain.dto.request.LoginRequest;
 import com.gobeyond.memebe.service.AuthService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
--- src/main/java/com/gobeyond/memebe/controller/UserController.java
+++ src/main/java/com/gobeyond/memebe/controller/UserController.java
@@ -2,11 +2,11 @@ package com.gobeyond.memebe.controller;
 
 import javax.validation.Valid;
 
-import com.gobeyond.memebe.domain.dto.UserDto;
 import com.gobeyond.memebe.domain.dto.request.RegistrationRequest;
 import com.gobeyond.memebe.domain.dto.response.RegistrationResponse;
-import com.gobeyond.memebe.service.AccountService;
 
+import com.gobeyond.memebe.service.AccountService;
+import com.gobeyond.memebe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private AccountService accountService;
+    private final AccountService accountService;
+
+    public UserController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<RegistrationResponse> registerUser(@Valid @RequestBody RegistrationRequest request) {
