056f9f0d8a52597a58b53f5f42391a69174cf578
IhebChiha <ihebfcb1899@gmail.com>
2021-11-01 22:20:24 +0100

    updating controller

--- src/main/java/com/gobeyond/memebe/controller/UserController.java
+++ src/main/java/com/gobeyond/memebe/controller/UserController.java
@@ -5,9 +5,7 @@ import javax.validation.Valid;
 import com.gobeyond.memebe.domain.dto.request.RegistrationRequest;
 import com.gobeyond.memebe.domain.dto.response.RegistrationResponse;
 
-import com.gobeyond.memebe.service.AccountService;
-import com.gobeyond.memebe.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.gobeyond.memebe.service.RegistrationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    private final AccountService accountService;
+    private final RegistrationService registrationService;
 
-    public UserController(AccountService accountService) {
-        this.accountService = accountService;
+    public UserController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegistrationResponse> registerUser(@Valid @RequestBody RegistrationRequest request) {
-        return ResponseEntity.ok(accountService.registerUser(request));
+        return ResponseEntity.ok(registrationService.registerUser(request));
     }
 }
