3e25b309f89d004571c60457633983953d3ed917
IhebChiha <ihebfcb1899@gmail.com>
2021-11-01 22:18:03 +0100

    deleting account entity

--- src/main/java/com/gobeyond/memebe/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.gobeyond.memebe.service;
-
-import com.gobeyond.memebe.domain.dto.request.RegistrationRequest;
-import com.gobeyond.memebe.domain.dto.response.RegistrationResponse;
-
-public interface AccountService {
-    RegistrationResponse registerUser(RegistrationRequest request);
-}
